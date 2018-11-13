7491fdc27f874f5b5e09c92997ed9430ffe569e2
Jonathan Xu <jonathanj.xu97@gmail.com>
2018-11-13 17:17:25 -0600

    Ref code

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
 const express = require('express');
 const path = require('path');
+const logger = require('./logger');
 
 const app = express();
+
+app.use(logger);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.use(function(req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
---
+++ logger.js
@@ -0,0 +1,7 @@
+function logger(req, res, next) {
+  const d = new Date();
+  console.log(d, req.path);
+  next();
+}
+
+module.exports = logger;
